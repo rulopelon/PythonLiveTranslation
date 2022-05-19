9845436b2aa6033e2e8ddc6474d8fd5a53f75fb5
rulopelon <48516780+rulopelon@users.noreply.github.com>
2022-05-19 16:51:00 +0200

    new organization

--- installation/install.sh
+++ installation/install.sh
@@ -1,7 +1,10 @@
 sudo apt update -Y
 sudo apt upgrade -Y
-sudo apt install git -Y
-sudo apt install python-pip -Y
+sudo apt install python3-pip -Y
 pip3 install --upgrade pip
 sudo apt install portaudio19-dev -Y
+pip3 install -r requirements.txt
+
+
+
 
